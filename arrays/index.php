bc0950f859b3b64606d9211c5bd206a1ef0c6581
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-03-09 23:33:23 -0300

    organizando arquivos

--- arrays/index.php
+++ arrays/index.php
@@ -1,13 +1,7 @@
 <?php
+
 require_once 'calculadora.php';
 
-$notas = [9, 3, 10, 5, 10];
+require_once 'calcular_media.php';
 
-$calculadora = new Calculadora();
-$media = $calculadora->calculaMedia($notas);
-
-if ($media > 0) {
-  echo "O cálculo da média é: $media";
-} else {
-  echo "Não foi possível calcular a média";
-}
+require_once 'menor_saldo.php';
