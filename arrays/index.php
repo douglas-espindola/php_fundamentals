2fdc6105547172ddd23467ff7c19c138acb94cd6
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-03-10 23:46:16 -0300

    organizado classes e programas

--- arrays/index.php
+++ arrays/index.php
@@ -1,7 +1,9 @@
 <?php
-
+// Classe
 require_once 'calculadora.php';
+require_once 'arrayUtils.php';
 
+// Programas
 require_once 'calcular_media.php';
-
 require_once 'menor_saldo.php';
+require_once 'remover_item_do_array.php';
