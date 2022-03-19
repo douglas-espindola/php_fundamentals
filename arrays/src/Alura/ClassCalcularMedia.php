ddf03112e9809d33d4d355b7d99efd981acc4b07
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-03-19 00:19:03 -0300

    criado pasta src

---
+++ arrays/src/Alura/ClassCalcularMedia.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Alura;
+
+class ClassCalcularMedia
+{
+  public function calculaMedia($notas)
+  {
+    $quantidade_notas = sizeof($notas);
+    if ($quantidade_notas > 0) {
+      $media = ($notas[0] + $notas[1] + $notas[2] + $notas[3] + $notas[4]) / $quantidade_notas;
+
+      return $media;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ arrays/src/Alura/classArrayUtils.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Alura;
+
+class classArrayUtils
+{
+  public static function remover($elmento, &$array)
+  {
+    $retornar_posicao = array_search($elmento, $array, true);
+
+    if (is_int($retornar_posicao)) {
+      unset($array[$retornar_posicao]);
+    } else {
+      echo "Não foi encontrato arrays";
+    }
+  }
+}
