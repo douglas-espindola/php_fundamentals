a8344320b9844d5ce7b358109088c309b70542d0
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-03-19 00:21:36 -0300

    Melhorado arquivo princial

--- arrays/index.php
+++ arrays/index.php
@@ -1,9 +1,9 @@
 <?php
-// Class
-require_once './class/ClassCalcularMedia.php';
-require_once './class/classArrayUtils.php';
 
-// Apps
-require_once './apps/calcula_media.php';
-require_once './apps/menor_saldo.php';
+namespace Alura;
+
+require 'autoload.php';
+
+// require_once './apps/calcula_media.php';
+// require_once './apps/menor_saldo.php';
 require_once './apps/remover_item_do_array.php';
