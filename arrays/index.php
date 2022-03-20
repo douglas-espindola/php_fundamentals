e0547e1cccfa2a56433969400d573ebbcfd86b45
Douglas Spíndola <douglas.spindola15@gmail.com>
2022-03-19 23:43:49 -0300

    finalizado pratica

--- arrays/index.php
+++ arrays/index.php
@@ -6,4 +6,6 @@ require 'autoload.php';
 
 // require_once './apps/calcula_media.php';
 // require_once './apps/menor_saldo.php';
-require_once './apps/remover_item_do_array.php';
+// require_once './apps/remover_item_do_array.php';
+
+require_once './apps/associacao_array.php';
